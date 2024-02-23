82ff4d79a07c2f2af1d1d76e7f0ab18804cffa27
ThreeE <zhuang.m.999@gmail.com>
2024-02-23 09:54:56 +0000

    新增不算警告的警告

--- setudownloader/define.py
+++ setudownloader/define.py
@@ -1,8 +1,10 @@
 import time
+
 NOTICE = 35
+NOTICE_WARN = 36
 
 def GetLogFileName(spider):
     current_timestamp = time.time()
     current_time_struct = time.localtime(current_timestamp)
-    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", current_time_struct)
-    return f"log/[{formatted_time}] {spider}.log"
+    formatted_time = time.strftime("%Y_%m_%d_%H_%M", current_time_struct)
+    return f"log/{formatted_time}_{spider}.log"
